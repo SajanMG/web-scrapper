f991f3ac8d270c84e833fccfd60114a97aa6521a
SajanMG <sajantamu04@gmail.com>
2026-09-17 10:25:34 +1000

    added new datasets for the grocery scraper and tested with positive result

--- src/scraper/models.py
+++ src/scraper/models.py
@@ -1,13 +1,18 @@
 from dataclasses import dataclass, asdict
 
-
 @dataclass
 class Product:
     name: str
-    price: float
+    current_price: float
     currency: str
     product_url: str
     source: str
+
+    regular_price: float | None = None
+    unit_price: float | None = None
+    unit: str | None = None
+    on_special: bool = False
+    available: bool = True
 
     def to_dict(self):
         return asdict(self)
